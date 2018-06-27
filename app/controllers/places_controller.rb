6798df1e333372164598adf1a35a5f4a9ee5473b
willphase <wherrington@gmail.com>
2018-06-27 08:03:40 -0400

    Initialize repository

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,22 @@
+class PlacesController < ApplicationController
+	def index
+		@places = Place.all
+	end
+	def show
+		@place = Place.find(params[:id])
+	end
+	def new
+	end
+
+	def create
+		@place = Place.new(place_params)
+ 
+  		@place.save
+  		redirect_to @place
+	end
+ 
+	private
+	  def place_params
+      	params.require(:place).permit(:title, :text)
+	  end
+end
