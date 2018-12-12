3029df02fd483fbe51eadb70b1edb1f593c60ba1
willphase <wherrington@gmail.com>
2018-12-12 07:26:50 -0500

    did this work?

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_06_27_111300) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "text"
